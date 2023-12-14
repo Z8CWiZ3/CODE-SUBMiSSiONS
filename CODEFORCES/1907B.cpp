9a172cc7627663c651e94b713a5bcf59780f8dbb
MZ Anjum HQ Heemel <153530948+Z8CWiZ3@users.noreply.github.com>
2023-12-14 21:22:57 +0600

    83CFX

---
+++ CODEFORCES/1907B.cpp
@@ -0,0 +1,122 @@
+// || ------------------------------------------------- || //
+// || ------   (BRAiN == DEAD) ? SLEEP : CODE!   ------ || //
+// || ------------------------------------------------- || //
+// || ---   !NOTHiNG iZ iMPOSSiBLE , KEEP TRYiNG!   --- || //
+// || ------------------------------------------------- || //
+// || -----------------   (Z8CWiZ3)   ----------------- || //
+// || ---------   ! KEEP ME iN YOUR HEART !   --------- || //
+// || ------------------------------------------------- || //
+
+#include                                      <bits/stdc++.h>
+using                   namespace                        std;
+
+// || ------------------------------------------------- || //
+
+#define XYZ                     ios_base::sync_with_stdio(0);
+#define YZX                                       cin.tie(0),
+#define ZXY                                      cout.tie(0);
+
+// || ------------------------------------------------- || //
+
+#define ull                                unsigned long long
+#define uin                                      unsigned int
+#define ll                                          long long
+#define db                                             double
+
+// || ------------------------------------------------- || //
+
+#define MIN                                           INT_MIN
+#define MAX                                           INT_MAX
+
+// || ------------------------------------------------- || //
+
+#define str                                            string
+#define ins                                            insert
+#define pb                                          push_back
+#define pf                                         push_front
+#define fs                                              first
+#define sc                                             second
+#define ft                                            front()
+#define bk                                             back()
+#define brk                                             break
+#define con                                          continue
+
+// || ------------------------------------------------- || //
+
+#define gcd(X, Y)                                 __gcd(X, Y)
+#define lcm(X, Y)                         (X * Y) / gcd(X, Y)
+#define lb(X, Y)                     lower_bound(every(X), Y)
+#define ub(X, Y)                     upper_bound(every(X), Y)
+#define src(X, Y)                  binary_search(every(X), Y)
+
+// || ------------------------------------------------- || //
+
+#define en                                               endl
+#define sz                                             size()
+#define C(XYZ)                                    cout << XYZ
+#define CE(XYZ)                             cout << XYZ << en
+#define entire(XYZ)                    XYZ.begin(), XYZ.end()
+#define fpve(X, Y, Z)      for (int i = X; i <= Y; i = i + Z)
+#define fnve(X, Y, Z)      for (int i = X; i >= Y; i = i - Z)
+
+// || ------------------------------------------------- || //
+
+#define ___TC                                 ll t; cin >> t;
+#define ___O_SOLN                                   z8cwiz3()
+#define ___M_SOLN                 ___TC while (t--) z8cwiz3()
+
+// || ------------------------------------------------- || //
+
+void z8cwiz3()
+{
+    str s;
+    cin >> s;
+    stack<int> lower_case;
+    stack<int> upper_case;
+    for (int i = 0; i < s.sz; i++)
+    {
+        if (s[i] != 'B' && s[i] != 'b')
+        {
+            if (s[i] >= 'a' && s[i] <= 'z')
+                lower_case.push(i);
+            else
+                upper_case.push(i);
+        }
+        else if (s[i] == 'B')
+        {
+            if (!upper_case.empty())
+                upper_case.pop();
+        }
+        else
+        {
+            if (!lower_case.empty())
+                lower_case.pop();
+        }
+    }
+    vector<int> res;
+    while (!lower_case.empty())
+    {
+        res.pb(lower_case.top());
+        lower_case.pop();
+    }
+    while (!upper_case.empty())
+    {
+        res.pb(upper_case.top());
+        upper_case.pop();
+    }
+    sort(entire(res));
+    for (int i = 0; i < res.sz; i++)
+        cout << s[res[i]];
+    cout << en;
+}
+
+// || ------------------------------------------------- || //
+
+int main()
+{
+    XYZ YZX ZXY
+
+        ___M_SOLN;
+
+    return 0;
+}
